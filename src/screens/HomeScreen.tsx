da69c26dabcd90a402dcddb62ddb819a1b37ea7e
Oliver Visoso <ovisoso@ucol.mx>
2023-01-15 16:04:16 -0600

    Peticion a la Api

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
-import { Text, View } from 'react-native';
+import { ActivityIndicator, Text, View } from 'react-native';
+import useMovies from '../hooks/useMovies';
 
 const HomeScreen = () => {
+
+    const { peliculasEnCine, isLoading } = useMovies()
+    // console.log(peliculasEnCine[0]?.title)
+
+    if (isLoading) {
+        return (
+            <View style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
+                <ActivityIndicator color="purple" size={100} />
+            </View>
+        )
+    }
+
   return (
     <View>
         <Text>HomeScreen</Text>
