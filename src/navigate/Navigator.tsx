190498b7dfb6ede8d87bd9e70c06f495a5fe9f75
Oliver Visoso <ovisoso@ucol.mx>
2023-01-19 17:51:28 -0600

    Navegacion a Details

--- src/navigate/Navigator.tsx
+++ src/navigate/Navigator.tsx
@@ -2,8 +2,14 @@ import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
 import DetailsScreen from '../screens/DetailsScreen';
+import { Movie } from '../interfaces/movieInterface';
 
-const Stack = createStackNavigator();
+export type RootStackParams = {
+  Home: undefined,
+  Details: Movie 
+}
+
+const Stack = createStackNavigator<RootStackParams>();
 
 export const Navigator = () => {
   return (
--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,7 +1,15 @@
+import { StackScreenProps } from '@react-navigation/stack';
 import React from 'react'
 import { View, Text } from 'react-native';
+// import { Movie } from '../interfaces/movieInterface';
+import { RootStackParams } from '../navigate/Navigator';
 
-const Details = () => {
+interface Props extends StackScreenProps<RootStackParams, 'Details'> {}
+
+const Details = ( { route }: Props) => {
+  
+  const movie = route.params
+  
   return (
     <View>
         <Text>Details</Text>
