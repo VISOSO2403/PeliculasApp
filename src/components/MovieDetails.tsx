18a69e0e549adf519a1b38d1f5dfd5d819d0d0af
Oliver Visoso <ovisoso@ucol.mx>
2023-01-30 19:42:38 -0600

    Actors cards

--- src/components/MovieDetails.tsx
+++ src/components/MovieDetails.tsx
@@ -4,22 +4,59 @@ import { MovieFull } from '../interfaces/movieInterface';
 import { Cast } from '../interfaces/creditsInterface';
 
 import Icon from 'react-native-vector-icons/Ionicons'
+import CastItem from './CastItem';
 
 interface Props {
-    movieFull: MovieFull
-    cast: Cast[]
+  movieFull: MovieFull
+  cast: Cast[]
 }
 
-const MovieDetails = ( { movieFull, cast }:Props ) => {
+const MovieDetails = ({ movieFull, cast }: Props) => {
   return (
     <>
-        <View style={{ marginHorizontal: 20 }}>
-            <Icon 
-                name="star-outline"
-                color="gray"
-                size={ 16 }
-            />
+      <View style={{ marginHorizontal: 20 }}>
+        <View style={{ flexDirection: 'row', marginRight: 10 }}>
+
+          <Icon
+            name="star-outline"
+            color="gray"
+            size={16}
+          />
+          <Text> {movieFull.vote_average}</Text>
+
+          <Text style={{ marginLeft: 3}}>
+            - {movieFull.genres.map(g => g.name).join(', ')}
+          </Text>
+
         </View>
+
+        {/* Historia */}
+        <Text style={{ fontSize: 23, marginTop: 10, fontWeight: 'bold'}}>
+          Historia
+        </Text>
+
+        <Text style={{ fontSize: 16 }}>
+          { movieFull.overview }
+        </Text>
+
+        {/* Presupuesto */}
+        <Text style={{ fontSize: 23, marginTop: 10, fontWeight: 'bold'}}>
+          Presupuesto
+        </Text>
+
+        <Text style={{ fontSize: 18 }}>
+          {movieFull.budget.toLocaleString('en',{ style: "currency", currency: "USD"})}
+        </Text>
+      </View>
+
+      {/* Casting */}
+      <View style={{ marginTop: 10, marginBottom: 100}}>
+        <Text style={{ fontSize: 23, marginTop: 10, fontWeight: 'bold', marginHorizontal: 20}}>
+          Actores
+        </Text>
+        <CastItem actor={ cast[0]}/>
+
+      </View>
     </>
   )
 }
